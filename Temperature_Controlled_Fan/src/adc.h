e166476b2d05846ca2b3ec195197343554cdf74e
Mostafa Mahmoud <72917420+mostsfamahmoud@users.noreply.github.com>
2022-10-08 16:35:09 +0200

    Update ADC Header File

--- Temperature_Controlled_Fan/src/adc.h
+++ Temperature_Controlled_Fan/src/adc.h
@@ -27,18 +27,22 @@
 #define ADC_REF_VOLT_VALUE    2.56
 
 
+/* Enum ADC_ReferenceVoltage to select the Reference Voltage of the ADC */
 typedef enum{
 	AREF,AVCC,RESERVED,INTERNAL_VREF
 }ADC_ReferenceVoltage;
 
+/* Enum ADC_Prescaler to select the divison factor between F_CPU and F_ADC */
 typedef enum{
 	FCPU_2,FCPU_4 = 2,FCPU_8,FCPU_16,FCPU_32,FCPU_64,FCPU_128
 }ADC_Prescaler;
 
+/* Enum ADC_Channel_ID to select which ADC Channel (0 --> 7) will be used */
 typedef enum{
 	ADC0,ADC1,ADC2,ADC3,ADC4,ADC5,ADC6,ADC7
 }ADC_Channel_ID;
 
+/* Struct ADC_ConfigType to make ADC driver Configurable by selecting the suitable Reference Voltage & Prescaler */
 typedef struct{
  ADC_ReferenceVoltage ref_volt;
  ADC_Prescaler prescaler;
@@ -50,11 +54,11 @@ typedef struct{
 
 /*
  * Description:
- *         Function responsible for initialize the ADC driver.
+ *     Function responsible for initialize the ADC driver.
  * Inputs:
- *         Pointer to Constant Struct of type (ADC_ConfigType) to dynamically configure Ref_Volt & Prescaler of ADC
+ *     Pointer to Constant Struct of type (ADC_ConfigType) to dynamically configure Ref_Volt & Prescaler of ADC
  * Return:
- *         None
+ *     None
  */
 void ADC_init(const ADC_ConfigType * Config_Ptr);
 
